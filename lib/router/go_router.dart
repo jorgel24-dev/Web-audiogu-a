3c4a1147358001c7fce0bf8f8f32842af28a0183
jorgel24-dev <jorgeliebananavarro@gmail.com>
2026-04-16 10:08:19 +0200

    feat: dashboard - presentation and shared layers

--- lib/router/go_router.dart
+++ lib/router/go_router.dart
@@ -1,3 +1,4 @@
+import 'package:audioguia_web/presentation/screens/dashboard_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -5,6 +6,9 @@ import 'package:go_router/go_router.dart';
 final appRouter = GoRouter(
   initialLocation: '/',
   routes: [
-
+    GoRoute(
+      path: '/',
+      builder: (context, state) => DashboardScreen(),
+    )
   ],
 );
